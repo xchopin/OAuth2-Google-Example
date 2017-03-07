e80a8b7d308115165a5a35457e4ee7323b829032
xchopin <xchopin54@gmail.com>
2017-03-08 00:40:08 +0100

    Created Resources

---
+++ resources.php
@@ -0,0 +1,6 @@
+<?php
+const GOOGLE_API = 'https://accounts.google.com/o/oauth2/v2/auth';
+const SCOPE = 'https://www.googleapis.com/auth/calendar';
+const URI = 'http://localhost/OAuth2-Google-Example/create_event.php';
+const CLIENT_ID = '763399929211-75h5794c4n2inqb2mlujr6fb20r61umf.apps.googleusercontent.com';
+const CLIENT_SECRET = 'T8uaP_ChOc0UE9BZuxg3lfkh';
